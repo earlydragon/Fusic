a882b259bb60c45117084f91042c10a823dc560e
earlydragon <ddesai@collegiateschool.org>
2014-12-14 12:56:57 -0500

    changes4

--- application_controller.rb
+++ application_controller.rb
@@ -2,26 +2,26 @@ require 'bundler'
 Bundler.require
 require_relative './models/scraper.rb'
 
-require 'bundler'
-Bundler.require
-require_relative './models/firebase.rb'
-require_relative './models/user.rb'
+# require 'bundler'
+# Bundler.require
+# require_relative './models/firebase.rb'
+# require_relative './models/user.rb'
 
 class MyApp < Sinatra::Base
-  DATABASE = FlatironBase.new("https://incandescent-inferno-7013.firebaseio.com/")
-  DATABASE.add_table("users", {"name" => "Placeholder Name", "email" => "placeholder@placeholder.com", "password" => "placeholderpassword"})
+#   DATABASE = FlatironBase.new("https://incandescent-inferno-7013.firebaseio.com/")
+#   DATABASE.add_table("users", {"name" => "Placeholder Name", "email" => "placeholder@placeholder.com", "password" => "placeholderpassword"})
 
-  get '/' do
-    @users = DATABASE.get_data
-    erb :index
-  end
+#   get '/' do
+#     @users = DATABASE.get_data
+#     erb :index
+#   end
 
-  post '/signup' do
-    DATABASE.add("users", {"name" => params[:name], "email" => params[:email], "password" => params[:password]})
-      redirect('/')
-  end
+#   post '/signup' do
+#     DATABASE.add("users", {"name" => params[:name], "email" => params[:email], "password" => params[:password]})
+#       redirect('/')
+#   end
 
-end
+
 
   get '/Login' do
     erb :Login
